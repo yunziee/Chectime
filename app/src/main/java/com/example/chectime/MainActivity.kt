0ca72d53f75d645a276302487c7b561fd603fc49
jeonminju2 <minju1806@naver.com>
2025-01-24 18:27:40 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/chectime/MainActivity.kt
+++ app/src/main/java/com/example/chectime/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.chectime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.chectime.Stopwatch.TimerFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         // BottomNavigationView 설정
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        
+
+        // 초기 화면은 일단 책장으로 설정
         loadFragment(CalendarFragment())
 
         // BottomNavigationView 클릭 리스너 설정
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_calendar -> loadFragment(CalendarFragment()) // 캘린더
                 R.id.nav_bookshelf -> loadFragment(BookshelfFragment()) // 책장
                 R.id.nav_routine -> loadFragment(AlarmFragment()) // 루틴
-                R.id.nav_timer -> loadFragment(TimerFragment()) // 타이머
+                R.id.nav_timer -> loadFragment(TimeFragment()) // 타이머
 //                R.id.nav_settings -> loadFragment(SettingsFragment()) // 설정
                 else -> false
             }
