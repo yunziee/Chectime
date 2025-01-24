48d63c3a72cad9b86ff724527d28f98a9c24b837
yunziee <yxnzxy@gmail.com>
2025-01-24 23:47:37 +0900

    settings 화면 추가

---
+++ app/src/main/java/com/example/chectime/Settings/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.chectime
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        return view
+    }
+
+}
