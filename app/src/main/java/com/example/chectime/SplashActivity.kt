774293fa73f82e7ebc17f28024b44a0170651cac
j6hnn1y6 <rkdfkdnf3@naver.com>
2025-02-03 11:47:16 +0900

    Icon & Splash

---
+++ app/src/main/java/com/example/chectime/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.chectime
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(3000) // 3초 딜레이
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
